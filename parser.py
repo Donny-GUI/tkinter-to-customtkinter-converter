46c76a4bd4e3b431cfbc8ef3c4d12261c6ce6c19
Donny <108424001+Donny-GUI@users.noreply.github.com>
2024-02-13 19:41:55 -0800

    modified:   parser.py

--- parser.py
+++ parser.py
@@ -2,39 +2,40 @@ import argparse
 
 
 argument_parser = argparse.ArgumentParser(description='Convert Tkinter scripts to Customtkinter scripts',
-                                     prog="tk2ctk",
-                                     usage="tk2ctk [Target] [-h] [-o Output] [-l] [-v] [-m Target Target Target Target ... ]",
-                                     add_help=False,
-                                     exit_on_error=False
-                                     )
+    prog="tk2ctk",
+    usage="tk2ctk [Target] [-h] [-o Output] [-l] [-v] [-m Target Target Target Target ... ]",
+    add_help=False,
+    exit_on_error=False
+    )
 argument_parser.add_argument('Target', 
-                    help='Input file path for Tkinter script',
-                    metavar="TARGET",
-                    nargs="?"
-                    )
+    help='Input file path for Tkinter script',
+    metavar="TARGET",
+    nargs="?"
+    )
 argument_parser.add_argument('-h', '--help', 
-                    dest="Help", 
-                    action="store_true", 
-                    help='Show this help message and exit')
+    dest="Help", 
+    action="store_true", 
+    help='Show this help message and exit')
 argument_parser.add_argument('-o', '--outfile', 
-                    dest="Output",
-                    nargs="?", 
-                    metavar="OUTFILE", 
-                    const='', 
-                    help='Specify output file')
+    dest="Output",
+    nargs="?", 
+    metavar="OUTFILE", 
+    const='', 
+    help='Specify output file')
 argument_parser.add_argument('-v', '--verbose', 
-                    dest="Verbose", 
-                    action='store_true',  
-                    help='Operate with verbosity')
+    dest="Verbose", 
+    action='store_true',  
+    help='Operate with verbosity')
 argument_parser.add_argument('-l', '--listboxes', 
-                    dest="Listboxes", 
-                    action='store_true', 
-                    help='Convert listboxes to custom listboxes')
+    dest="Listboxes", 
+    action='store_true', 
+    help='Convert listboxes to custom listboxes')
 argument_parser.add_argument('-m', '--multiple', 
-                    dest="Multiple", 
-                    nargs='+',  
-                    help="Specify multiple targets")
+    dest="Multiple", 
+    nargs='+',  
+    help="Specify multiple targets")
 argument_parser.add_argument('-e', '--examples', 
-                    dest="Examples", 
-                    action="store_true",
-                    help="Show examples for additional help")
+    dest="Examples", 
+    action="store_true",
+    help="Show examples for additional help")
+
